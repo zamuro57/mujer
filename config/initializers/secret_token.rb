5d8bbf4d0ef8a169ade701af86eb74c44b5c12d0
Sergio Marin <higher.vnf@gmail.com>
2012-02-06 19:43:30 -0430

    project reset

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+W::Application.config.secret_token = '1b48ac30edbd7e162cf0cee2b81badcc3655a8dc11d2af3f88d361d49c1e2d387fbb2a8f9da023dc95fd4cd9ffeb9c57c55a49eb63211ffc919bdf5f2caaa881'
